33e101f2faf81764599b81bda2b1053413782aed
Andrew MacMurray <a.macmurray@icloud.com>
2017-11-03 12:58:42 +0000

    parses second csv and combines data to be posted into elasticsearch

--- src/Minecart.hs
+++ src/Minecart.hs
@@ -5,12 +5,17 @@ module Main where
 
 import           Control.Applicative    ((<|>))
 import           Control.Monad.IO.Class (liftIO)
-import           Data.Aeson             (object, (.=))
+import           Data.Aeson             (FromJSON, ToJSON, defaultOptions,
+                                         genericParseJSON, genericToJSON,
+                                         object, (.=))
 import qualified Data.Aeson             as A
 import           Data.ByteString.Lazy   (ByteString)
 import qualified Data.ByteString.Lazy   as BL
 import           Data.Csv               hiding (Parser, (.=))
 import qualified Data.Csv.Streaming     as S
+import qualified Data.IntMap            as IM
+import           Data.Maybe             (fromMaybe)
+import           Data.Monoid            ((<>))
 import           Data.Text              (pack)
 import           Data.Text.Lazy         (Text)
 import           Data.Time              (Day, fromGregorian)
@@ -21,7 +26,6 @@ import           Network.HTTP.Client    (defaultManagerSettings)
 import           System.Environment     (getArgs)
 import           Text.Trifecta          (Parser, Result (..), char, integer,
                                          parseString, string, try)
-
 data Post =
   Post {
     userId    :: Int
@@ -34,14 +38,29 @@ data Post =
   , date      :: Maybe Day
   , visible   :: Bool
   , moderated :: Bool
+  , entities  :: [Entity]
   } deriving (Eq, Show, Generic)
 
+data Entity =
+  Entity {
+    messageId'         :: Int
+  , name               :: String
+  , salience           :: Double
+  , sentimentMagnitude :: Double
+  , sentimentScore     :: Double
+  } deriving (Eq, Show, Generic)
 
-instance A.ToJSON Post where
-  toJSON = A.genericToJSON A.defaultOptions
+instance ToJSON Entity where
+  toJSON = genericToJSON defaultOptions
 
-instance A.FromJSON Post where
-  parseJSON = A.genericParseJSON A.defaultOptions
+instance ToJSON Post where
+  toJSON = genericToJSON defaultOptions
+
+instance FromJSON Entity where
+  parseJSON = genericParseJSON defaultOptions
+
+instance FromJSON Post where
+  parseJSON = genericParseJSON defaultOptions
 
 instance FromNamedRecord Post where
   parseNamedRecord r = Post <$> r .: "UserID"
@@ -54,6 +73,14 @@ instance FromNamedRecord Post where
                             <*> (toDay <$> (r .: "CreationDate"))
                             <*> (toBool <$> (r .: "Visible"))
                             <*> (toBool <$> (r .: "Moderated"))
+                            <*> return []
+
+instance FromNamedRecord Entity where
+  parseNamedRecord r = Entity <$> r .: "MessageId"
+                              <*> r .: "EntityName"
+                              <*> r .: "Salience"
+                              <*> r .: "SentimentMagnitude"
+                              <*> r .: "SentimentScore"
 
 data PostMapping = PostMapping
 
@@ -62,19 +89,19 @@ instance A.ToJSON PostMapping where
     object
       [ "properties" .=
           object [
-            "userId"    .= object ["type" .= ("keyword" :: Text)]
-          , "topidId"   .= object ["type" .= ("keyword" :: Text)]
-          , "forumId"   .= object ["type" .= ("keyword" :: Text)]
-          , "messageId" .= object ["type" .= ("keyword" :: Text)]
+            "userId"    .= object ["type" .= ("integer" :: Text)]
+          , "topidId"   .= object ["type" .= ("integer" :: Text)]
+          , "forumId"   .= object ["type" .= ("integer" :: Text)]
+          , "messageId" .= object ["type" .= ("integer" :: Text)]
           , "username"  .= object ["type" .= ("keyword" :: Text)]
           , "subject"   .= object ["type" .= ("text" :: Text)]
           , "body"      .= object ["type" .= ("text" :: Text)]
           , "date"      .= object ["type" .= ("date" :: Text)]
           , "visible"   .= object ["type" .= ("visible" :: Text)]
           , "moderated" .= object ["type" .= ("moderated" :: Text)]
+          , "entities"  .= object ["type" .= ("nested" :: Text)]
           ]
       ]
-
 
 toBool :: ByteString -> Bool
 toBool "TRUE"  = True
@@ -122,27 +149,43 @@ postMapping   = MappingName "post"
 server        = Server "http://localhost:9200"
 
 
-mkBulkStream :: S.Records Post -> V.Vector BulkOperation
-mkBulkStream = snd . foldr incId (0, V.empty)
+mkBulkStream :: S.Records Entity -> S.Records Post -> V.Vector BulkOperation
+mkBulkStream entities = snd . foldr incId (0, V.empty)
   where
-    incId post (n, stream) = (n + 1, V.snoc stream $ mkBulkOperation n post)
+    enMap = messageEntities entities
+    incId post (n, stream) = (n + 1, V.snoc stream $ mkBulkOperation n enMap post)
 
-mkBulkOperation :: Integer -> Post -> BulkOperation
-mkBulkOperation n post = BulkIndex gbIndex postMapping (DocId . pack $ show n) $ toJSON post
+mkBulkOperation :: Integer -> IM.IntMap [Entity] -> Post -> BulkOperation
+mkBulkOperation n enMap post = BulkIndex gbIndex postMapping (DocId . pack $ show n) $ toJSON updatedPost
+  where
+    updatedPost = setEntities post . fromMaybe [] . IM.lookup (messageId post) $ enMap
 
+messageEntities :: S.Records Entity -> IM.IntMap [Entity]
+messageEntities = foldr accum IM.empty
+  where accum a = IM.insertWith (++) (messageId' a) [a]
+
+setEntities :: Post -> [Entity] -> Post
+setEntities post xs = post { entities = xs }
+
+handleElastic :: S.Records Entity -> S.Records Post -> BH IO ()
+handleElastic entities posts = do
+  liftIO $ putStrLn "resetting index"
+  _ <- deleteIndex gbIndex
+  _ <- createIndex indexSettings gbIndex
+  _ <- putMapping gbIndex postMapping PostMapping
+  liftIO $ putStrLn "posting to elasticsearch"
+  _ <- bulk $ mkBulkStream entities posts
+  _ <- refreshIndex gbIndex
+  liftIO $ putStrLn "done"
 
 main :: IO ()
 main = do
-  csvPath <- head <$> getArgs
-  x       <- BL.readFile csvPath
+  csvsPath <- head <$> getArgs
+  x <- BL.readFile $ csvsPath <> "gb-forum.csv"
+  y <- BL.readFile $ csvsPath <> "gb-forum-entities.csv"
   case S.decodeByName x of
-    Left err      -> putStrLn err
-    Right (_, xs) -> runBH' $ do
-      liftIO $ putStrLn "resetting index"
-      _ <- deleteIndex gbIndex
-      _ <- createIndex indexSettings gbIndex
-      _ <- putMapping gbIndex postMapping PostMapping
-      liftIO $ putStrLn "posting to elasticsearch"
-      _ <- bulk $ mkBulkStream xs
-      _ <- refreshIndex gbIndex
-      liftIO $ putStrLn "done"
+    Left err         -> putStrLn err
+    Right (_, posts) ->
+      case S.decodeByName y of
+        Left err            -> putStrLn err
+        Right (_, entities) -> runBH' $ handleElastic entities posts
