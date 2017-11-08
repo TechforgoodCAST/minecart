ba1d7b9820dc065827c1a8d6278d33c1c800bbbb
Andrew MacMurray <a.macmurray@icloud.com>
2017-11-08 20:45:20 +0000

    google config embedded in readerT

--- src/Cloudcart.hs
+++ src/Cloudcart.hs
@@ -3,43 +3,44 @@
 module Main where
 
 import           Cloudcart.Types
-import           Control.Monad          (forever)
-import           Control.Monad.IO.Class (liftIO)
+import           Control.Monad              (forever)
+import           Control.Monad.IO.Class     (liftIO)
+import           Control.Monad.Trans.Reader (ReaderT, ask, runReaderT)
 import           Data.Aeson
-import           Data.ByteString.Lazy   (ByteString)
-import qualified Data.ByteString.Lazy   as BL
-import           Data.Csv               (ToRecord)
-import qualified Data.Csv               as C
-import qualified Data.Csv.Streaming     as S
-import           Data.Monoid            ((<>))
-import           Data.Text.Lazy         (Text)
+import           Data.ByteString.Lazy       (ByteString)
+import qualified Data.ByteString.Lazy       as BL
+import           Data.Csv                   (ToRecord)
+import qualified Data.Csv                   as C
+import qualified Data.Csv.Streaming         as S
+import           Data.Monoid                ((<>))
+import           Data.Text.Lazy             (Text)
 import           Network.HTTP.Simple
 import           Pipes
-import           System.Environment     (getArgs, lookupEnv)
-import           System.Exit            (exitFailure, exitSuccess)
+import           System.Environment         (getArgs, lookupEnv)
+import           System.Exit                (exitFailure, exitSuccess)
 
 -- Pipes and Google Requests
 
-type GooglePipe a = Pipe Post [a] IO ()
+type GoogleReader = ReaderT GoogleConfig IO
 
-storeEntities :: Foldable f => GoogleConfig -> f Post -> Effect IO ()
+storeEntities :: Foldable f => f Post -> Effect GoogleReader ()
 storeEntities = storeGoogleData $ mkGooglePipe googleEntityRequest
 
-storeSentiments :: Foldable f => GoogleConfig -> f Post -> Effect IO ()
+storeSentiments :: Foldable f => f Post -> Effect GoogleReader ()
 storeSentiments = storeGoogleData $ mkGooglePipe googleSentimentRequest
 
 storeGoogleData :: (Foldable f, ToRecord a)
-                => (GoogleConfig -> GooglePipe a)
-                -> GoogleConfig
+                => Pipe Post [a] GoogleReader ()
                 -> f Post
-                -> Effect IO ()
-storeGoogleData googlePipe config posts =
+                -> Effect GoogleReader ()
+storeGoogleData googlePipe posts =
   each posts
-    >-> googlePipe config
-    >-> writeToCsv config
+    >-> googlePipe
+    >-> writeToCsv
 
-writeToCsv :: ToRecord a => GoogleConfig -> Consumer [a] IO ()
-writeToCsv config = forever $ do
+writeToCsv :: ToRecord a => Consumer [a] GoogleReader ()
+writeToCsv = forever $ do
+  config <- lift ask
   let newFile = outputPath config <> outputFile (requestType config)
   await >>= (liftIO . BL.appendFile newFile . C.encode)
 
@@ -48,28 +49,31 @@ outputFile EntitySentimentAnalysis = "gb-forum-entities.csv"
 outputFile SentimentAnalysis       = "gb-forum-sentiments.csv"
 
 mkGooglePipe :: ToRecord a
-             => (GoogleConfig -> Post -> IO [a])
-             -> GoogleConfig
-             -> GooglePipe a
-mkGooglePipe googleRequest config = forever $ await >>= (liftIO . googleRequest config) >>= yield
+             => (Post -> GoogleReader [a])
+             -> Pipe Post [a] GoogleReader ()
+mkGooglePipe googleRequest = forever $ await >>= (lift . googleRequest) >>= yield
 
-googleEntityRequest :: GoogleConfig -> Post -> IO [Entity]
+googleEntityRequest :: Post -> GoogleReader [Entity]
 googleEntityRequest = transformGoogleResponse setPostId
   where
     setPostId n (Entities xs) = map (\e -> e { entityPostId = n }) xs
 
-googleSentimentRequest :: GoogleConfig -> Post -> IO [SentenceMeta]
+googleSentimentRequest :: Post -> GoogleReader [SentenceMeta]
 googleSentimentRequest = transformGoogleResponse setSentenceData
   where
     setSentenceData n (Sentences _ dss dsm xs) = map (SentenceMeta n dss dsm) xs
 
-transformGoogleResponse :: (FromJSON a, ToRecord b) => (Int -> a -> [b]) -> GoogleConfig -> Post -> IO [b]
-transformGoogleResponse transformF config post = do
-  res <- googleRequest config post
+transformGoogleResponse :: (FromJSON a, ToRecord b)
+                        => (Int -> a -> [b])
+                        -> Post
+                        -> GoogleReader [b]
+transformGoogleResponse transformF post = do
+  res <- googleRequest post
   return $ transformF (postId post) res
 
-googleRequest :: FromJSON a => GoogleConfig -> Post -> IO a
-googleRequest config post = do
+googleRequest :: FromJSON a => Post -> GoogleReader a
+googleRequest post = do
+  config <- ask
   let pId   = postId post
       key   = apiKey config
       type' = requestTypeString $ requestType config
@@ -78,10 +82,11 @@ googleRequest config post = do
               <> type'
               <> "?key="
               <> key
-  putStrLn $ "fetching data for: " ++ show pId
-  req <- body' <$> parseRequest rawR
-  res <- httpJSON req
-  return $ getResponseBody res
+  liftIO $ do
+    putStrLn $ "fetching data for: " ++ show pId
+    req <- body' <$> parseRequest rawR
+    res <- httpJSON req
+    return $ getResponseBody res
 
 requestTypeString :: GoogleRequestType -> String
 requestTypeString EntitySentimentAnalysis = "analyzeEntitySentiment"
@@ -110,9 +115,11 @@ getCsvPath = do
 
 requestHandler :: Foldable f => GoogleConfig -> f Post -> IO ()
 requestHandler config@(GoogleConfig _ reqType _) posts =
+  let runner ef = runReaderT (runEffect ef) config
+  in
   case reqType of
-    EntitySentimentAnalysis -> runEffect $ storeEntities config posts
-    SentimentAnalysis       -> runEffect $ storeSentiments config posts
+    EntitySentimentAnalysis -> runner $ storeEntities posts
+    SentimentAnalysis       -> runner $ storeSentiments posts
 
 collectGoogleCloudData :: IO ()
 collectGoogleCloudData = do
